fixed gradient explosion problem laedr. Added checkpointing and changed

src/ml/data_loader.py
@@ -132,10 +132,13 @@ class Data_reader():
 		self.trainData = []
 		self.testData = []
 
+        def process_image(self, img):
+            return np.float32(img) / 127.5 - 1.
+
 	def set_traintest_split(self,split = 0.7):
 		total_dataSize = len(self.data)
-		self.trainData = self.data[0: math.floor(total_dataSize * split)]
-		self.testData = self.data[math.ceil(total_dataSize * split): total_dataSize]
+		self.trainData = self.data[0: int(math.floor(total_dataSize * split))]
+		self.testData = self.data[int(math.ceil(total_dataSize * split)): total_dataSize]
 
 	def load_data(self):
 		print('Loading data...')
@@ -154,6 +157,7 @@ class Data_reader():
 				for i in glob.glob("./data/TSKinFace_Data/TSKinFace_Cropped/" + relation + "/" + relation + "-" + str(j) + "-*.jpg"):
 					img = cv2.cv2.imread(i)
 					img = cv2.cv2.resize(img, (128, 128))
+                                        img = self.process_image(img)
 					i = i.split('/')[-1]
 					i = i.split('.')[0]
 					i = i.split('-')
@@ -173,7 +177,10 @@ class Data_reader():
 					imgPath = './data/TSKinFace_Data/TSKinFace_Cropped/' + rel + '/' + rel + "-" + str(rand_idx) + "-" + child_role + ".jpg"
 					neg_child_img = cv2.cv2.imread(imgPath)
 					neg_child_img = cv2.cv2.resize(neg_child_img, (128, 128))
-					neg_children.append(neg_child_img)
+                                        neg_child_img = self.process_image(neg_child_img)
+
+					neg_children = neg_child_img
+                                        break
 				data.append([father, mother, mother_likedness, pos_child, neg_children])
 			self.data = data
 			print('Load finished.')

src/ml/laedr/layers2.py
@@ -126,7 +126,7 @@ class conv2D(Layer):
 				x_init = tf.nn.conv2d(self.x,self.W,stride,pad,dilations=dilation_rate)
 				m_init, v_init = tf.nn.moments(x_init,[0,1,2])
 				s_init = 1. / tf.sqrt(v_init + 1e-8)
-				s = tf.get_variable('weight_scale',dtype=tf.float32,initializer=s_init)
+				s = tf.compat.v1.get_variable('weight_scale',dtype=tf.float32,initializer=s_init)
 				self.S = s.initialized_value()
 				self.S = tf.reshape(self.S,[1,1,1,outchn])
 				self.W = self.S *self.W

src/ml/laedr/modeleag.py
@@ -1,8 +1,8 @@
-import layers2 as L
+import layers2 as L 
 import tensorflow as tf 
-config = tf.ConfigProto()
+config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth=True
-tf.enable_eager_execution(config=config)
+tf.compat.v1.enable_eager_execution(config=config)
 import numpy as np 
 import os 
 import random
@@ -242,9 +242,9 @@ class Saver():
 		self.obj.m = self.mod
 		self.optim = optim 
 		if optim is None:
-			self.ckpt = tf.train.Checkpoint(model=self.obj, optimizer_step=tf.train.get_or_create_global_step())
+			self.ckpt = tf.train.Checkpoint(model=self.obj, optimizer_step=tf.compat.v1.train.get_or_create_global_step())
 		else:
-			self.ckpt = tf.train.Checkpoint(optimizer=optim, model=self.obj, optimizer_step=tf.train.get_or_create_global_step())
+			self.ckpt = tf.train.Checkpoint(optimizer=optim, model=self.obj, optimizer_step=tf.compat.v1.train.get_or_create_global_step())
 	
 	def save(self, path):
 		print('Saving model to path:',path)
@@ -262,11 +262,11 @@ class Saver():
 				print('Checkpoint:', last_ckpt)
 				if last_ckpt is None:
 					print('No model found in checkpoint.')
-					print("please train the feature extractor model first.")
+					print('Model will auto-initialize after first iteration.')
 				self.ckpt.restore(last_ckpt)
 			else:
 				self.ckpt.restore(path)
-			print('Finish loading.')
+			print('Finish loading missing child model.')
 		except Exception as e:
 			print('Model restore failed, Exception:',e)
 			print('Model will auto-initialize after first iteration.')

src/ml/model.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from laedr import modeleag as laedrM
 from laedr import network as laedrNetwork
 import tensorflow as tf
@@ -9,11 +10,7 @@ pretrainedLaedrModelPath = './laedr/model/'
 class LAEDR_AIM(laedrM.Model):
     def initialize(self):
         self.encoder = laedrNetwork.EncoderNet()
-        self.decoder = laedrNetwork.DecoderNet()
-        self.dis_z = laedrNetwork.DiscriminatorZ()
-        self.age_classifier = laedrNetwork.AgeClassifier() 
-        self.dis_img = laedrNetwork.DiscriminatorPatch()
-        optim_default = tf.train.AdamOptimizer(0.0001)
+        optim_default = tf.compat.v1.train.AdamOptimizer(0.0001)
         saver = laedrM.Saver(self, optim_default)
         saver.restore(pretrainedLaedrModelPath)
 
@@ -23,7 +20,6 @@ class Missing_Child_Model:
     def __init__(self):
         self.LAEDR_model = LAEDR_AIM()
         self.evaluation_metrics = {}
-        self.step = None
 
         self.top_k_cache_id = None
         self.top_k = None
@@ -36,8 +32,8 @@ class Missing_Child_Model:
         mother_aif = self.LAEDR_model.encoder(batch_mothers)
         father_aif = self.LAEDR_model.encoder(batch_fathers)
         # stop gradient so that we don't backpropagate till here.
-        tf.stop_gradient(mother_aif)
-        tf.stop_gradient(father_aif)
+        mother_aif = tf.stop_gradient(mother_aif)
+        father_aif = tf.stop_gradient(father_aif)
 
         #TODO: implement attention here
 
@@ -45,10 +41,11 @@ class Missing_Child_Model:
         mf_aif_concatted = tf.concat([mother_aif, father_aif], 1)
 
         # this is the input-output NN. Matches mom and dad concated
-        layer1 = tf.layers.dense(mf_aif_concatted, 40, activation=tf.nn.tanh, use_bias=True)
-        layer2 = tf.layers.dense(layer1, 30, activation=tf.nn.tanh, use_bias=True)
-        layer3 = tf.layers.dense(layer2, 40, activation=tf.nn.tanh, use_bias=True)
-        model_output = tf.layers.dense(layer3, laedrNetwork.Z_DIM, activation=tf.nn.tanh, use_bias=True)
+        layer1 = tf.layers.dense(mf_aif_concatted, 60, kernel_regularizer=tf.contrib.layers.l2_regularizer(0.01), activation=tf.nn.tanh, use_bias=True)
+        layer1 = tf.layers.dense(mf_aif_concatted, 40, kernel_regularizer=tf.contrib.layers.l2_regularizer(0.01), activation=tf.nn.tanh, use_bias=True)
+        layer2 = tf.layers.dense(mf_aif_concatted, 35, kernel_regularizer=tf.contrib.layers.l2_regularizer(0.01), activation=tf.nn.tanh, use_bias=True)
+        layer3 = tf.layers.dense(mf_aif_concatted, 40, kernel_regularizer=tf.contrib.layers.l2_regularizer(0.01), activation=tf.nn.tanh, use_bias=True)
+        model_output = tf.layers.dense(layer3, laedrNetwork.Z_DIM, kernel_regularizer=tf.contrib.layers.l2_regularizer(0.01), activation=tf.nn.tanh, use_bias=True)
         return model_output
 
     def compute_cxent_loss(self, batch_fathers):
@@ -61,15 +58,19 @@ class Missing_Child_Model:
 
 
     def compute_triplet_loss(self, batch_fathers, batch_mothers, mother_likedness_array, batch_child_positives, batch_child_negatives):
-        positive_child_aifs = self.LAEDR_model.encoder(batch_child_positives)
-        negative_child_aifs = self.LAEDR_model.encoder(batch_child_negatives)
-        model_output = self.forward_pass(batch_fathers, batch_mothers, mother_likedness_array)
+        positive_child_aifs = tf.math.l2_normalize(self.LAEDR_model.encoder(batch_child_positives), axis=1)
+        negative_child_aifs = tf.math.l2_normalize(self.LAEDR_model.encoder(batch_child_negatives), axis=1)
+        model_output = tf.math.l2_normalize(self.forward_pass(batch_fathers, batch_mothers, mother_likedness_array), axis=1)
         # triplet loss on the AIFs 
         # triplet loss as introduced by VGGFACE : Maximising vector distance for unrelated pairs, and minimising otherwise.
+
         d_pos = tf.reduce_sum(tf.square(model_output - positive_child_aifs), 1)
         d_neg = tf.reduce_sum(tf.square(model_output - negative_child_aifs), 1)
+
         triplet_loss_margin = tf.Variable(0.01, name="triplet_loss_margin")
-        triplet_loss = tf.maximum(0., triplet_loss_margin + tf.reduce_sum(d_pos - d_neg))
+
+        triplet_loss = tf.maximum(0., triplet_loss_margin + d_pos - d_neg)
+        triplet_loss = tf.reduce_mean(triplet_loss)
 
         return triplet_loss
 
@@ -82,10 +83,15 @@ class Missing_Child_Model:
     # train one batch. Returns the batch loss.
     def train_one_step(self, optimizer, batch_fathers, batch_mothers, mother_likedness_array, batch_child_positives, batch_child_negatives):
     # TODO: also try RMSE loss of  the output and the child's AIF
+        """
         runner = optimizer.minimize(lambda: self.compute_triplet_loss(batch_fathers, batch_mothers, mother_likedness_array, batch_child_positives, batch_child_negatives))
-        #TODO: for some reason, 'runner' here is None. Why? Aren't we supposed to run to update the gradients?
-        #batch_loss = runner.run()
         batch_loss = self.compute_triplet_loss(batch_fathers, batch_mothers, mother_likedness_array, batch_child_positives, batch_child_negatives)
+        """
+        gradients, variables = zip(*optimizer.compute_gradients(lambda: self.compute_triplet_loss(batch_fathers, batch_mothers, mother_likedness_array, batch_child_positives, batch_child_negatives)))
+        gradients, _ = tf.clip_by_global_norm(gradients, 5.0)
+        optimizer.apply_gradients(zip(gradients, variables))
+        batch_loss = self.compute_triplet_loss(batch_fathers, batch_mothers, mother_likedness_array, batch_child_positives, batch_child_negatives)
+
         return batch_loss
 
 

src/ml/train_model.py
@@ -2,58 +2,57 @@
 
 from model import Missing_Child_Model
 import tensorflow as tf
-import data_loader
+import data_loader as data
+from laedr.modeleag import Saver
 
-EPOCHS = 5000
+EPOCHS = 500
 BATCHES_PER_EPOCH = 10
-BATCH_SIZE = 50 # this is the amount of samples, where each consists of 1 M, 1 F, 1 C_P  1C_N . 
+BATCH_SIZE = 40  # this is the amount of samples, where each consists of 1 M, 1 F, 1 C_P  1C_N . 
+TF_DEVICE="GPU:0"
 
 tboard_logdir="./tboard_logs"
 
 if __name__=='__main__':
 
+
     mcm = Missing_Child_Model()
 
-    optimizer = tf.train.AdamOptimizer(learning_rate=0.0001)
-    data_loader = data_reader()
+    optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=0.0001)
+    data_loader = data.Data_reader()
     data_loader.set_traintest_split(0.7)
 
     summary_writer = tf.contrib.summary.create_file_writer(tboard_logdir, flush_millis=4000)
+
+    model_saver = Saver(mcm, optimizer)
     with summary_writer.as_default(), tf.contrib.summary.always_record_summaries():
-        for i in range(EPOCHS):
-            avg_triplet_loss = 0
-            for j in range(BATCHES_PER_EPOCH):
-              	bf, bm, m_array, bcp, bcn = data_loader.get_next_train_batch(BATCH_SIZE)
+        with tf.device(TF_DEVICE):
+            for i in range(EPOCHS):
+                avg_triplet_loss = 0
+                for j in range(BATCHES_PER_EPOCH):
+                    bf, bm, m_array, bcp, bcn = data_loader.get_next_train_batch(BATCH_SIZE)
+                    batch_triplet_loss = mcm.train_one_step(optimizer, bf, bm, m_array, bcp, bcn)
+                    print("epoch {}, batch # {}, triplet loss {}".format(i, j, batch_triplet_loss))
+                    avg_triplet_loss += batch_loss
+                avg_triplet_loss /= (BATCHES_PER_EPOCH * 1.0)
 
-                batch_loss = mcm.train_one_step(optimizer, \
-                    bf, \
-                    bm, \
-                    m_array, \
-                    bcp, \
-                    bcn \
-                )
-                print("epoch {}, batch # {}, loss {}".format(i, j, batch_loss))
-                avg_triplet_loss += batch_loss
-            avg_triplet_loss /= (BATCHES_PER_EPOCH * 1.0)
+                bf, bm, m_array, bc = data_loader.get_next_test_batch(BATCH_SIZE)
+                # top 5
+                top_5_acc = mcm.evaluate_accuracy(BATCH_SIZE, bf, bm, m_array, bc, top_n = 5, cache_id="ep-{}".format(i))
 
+                # top 2
+                top_2_acc = mcm.evaluate_accuracy(BATCH_SIZE, bf, bm, m_array, bc, top_n = 2, cache_id="ep-{}".format(i))
 
-            bf, bm, m_array, bc = data_loader.get_next_test_batch(BATCH_SIZE)
-            # top 5
-            top_5_acc = mcm.evaluate_accuracy(BATCH_SIZE, bf, bm, m_array, bc, top_n = 5, cache_id="ep-{}".format(i))
+                # top 1
+                top_1_acc = mcm.evaluate_accuracy(BATCH_SIZE, bf, bm, m_array, bc, top_n = 1, cache_id="ep-{}".format(i))
+                print("TOP-5 ACC: {}, TOP-2 ACC: {}, TOP-1 ACC: {}".format(top_5_acc, top_2_acc, top_1_acc))
 
-            # top 2
-            top_2_acc = mcm.evaluate_accuracy(BATCH_SIZE, bf, bm, m_array, bc, top_n = 2, cache_id="ep-{}".format(i))
+                # we include the step here because we don't do global steps. Is this the right way to do it actually?
+                tf.contrib.summary.scalar('top_5_acc', top_5_acc, step=i)
+                tf.contrib.summary.scalar('top_2_acc', top_2_acc, step=i)
+                tf.contrib.summary.scalar('top_1_acc', top_1_acc, step=i)
+                tf.contrib.summary.scalar('avg_triplet_loss', avg_triplet_loss, step=i)
 
-            # top 1
-            top_1_acc = mcm.evaluate_accuracy(BATCH_SIZE, bf, bm, m_array, bc, top_n = 1, cache_id="ep-{}".format(i))
-            print("TOP-5 ACC: {}, TOP-2 ACC: {}, TOP-1 ACC: {}".format( \
-                top_5_acc, \
-                top_2_acc, \
-                top_1_acc  \
-            ))
+                # save the model every 2 epochs
+                if i > 0 and i % 2 == 0:
+                    model_saver.save("./model_checkpoints/ckpt-{}".format(i))
 
-            # we include the step here because we don't do global steps. Is this the right way to do it actually?
-            tf.contrib.summary.scalar('top_5_acc', top_5_acc, step=i)
-            tf.contrib.summary.scalar('top_2_acc', top_2_acc, step=i)
-            tf.contrib.summary.scalar('top_1_acc', top_1_acc, step=i)
-            tf.contrib.summary.scalar('avg_triplet_loss', avg_triplet_loss, step=i)